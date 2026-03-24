eac274e8bc4dc205eba0365daa4ccb5b3ecd7cbe
Mir Arsh Zaffar <mirarsh6119@gmail.com>
2026-03-24 19:22:26 +0530

    Redefined API endpoint and Port

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ app.use("/api/users", userRoutes);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected!"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000!");
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
